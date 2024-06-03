0cf9b83874bec90d08428a8fa2558dd54be19ca2
Mihai0209 <mihai.istrate@student.usv.ro>
2024-06-03 23:11:33 +0300

    The player can only move on normal tiles

--- SokobanEGC/Assets/Scripts/Map/Tile.cs
+++ SokobanEGC/Assets/Scripts/Map/Tile.cs
@@ -6,8 +6,8 @@ namespace EGC.Map
     public class Tile : MonoBehaviour
     {
         protected int _id;
-        protected GridPosition _position;
         protected GameObject _prefab;
+        public GridPosition Position;
 
        
     }
